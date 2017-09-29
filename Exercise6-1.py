e72194947ffb8c7e0ddd50b172fb7188fec82068
Phil McCown <pjlmac@gmail.com>
2017-09-29 11:22:51 -0400

    Initial commit

---
+++ Exercise6-1.py
@@ -0,0 +1,7 @@
+import pandas
+import matplotlib.pyplot as plt
+bball=pandas.read_table('UWvMSU_1-22-13.txt',delim_whitespace=True)
+UW=bball[bball.team=='UW']
+MSU=bball[bball.team=='MSU']
+
+plt.plot('UWscore','r-','MSUscore','g-')
