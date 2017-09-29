0afa63d3b7abed489d232c913002ccc44844f835
Katherine <bookaholic214@gmail.com>
2017-09-29 11:02:16 -0400

    Added blank matrix

--- exercise6.py
+++ exercise6.py
@@ -5,8 +5,10 @@
 
 #load the dataset
 import pandas
+import numpy
 bball = pandas.read_csv("UWvMSU_1-22-13.txt", sep='\t', lineterminator='\r')
-
+A=numpy.zeros((50,3))
+B=pandas.DataFrame(A,columns=['Team','Time','CumeScore'])
 print bball
 #hello
 
