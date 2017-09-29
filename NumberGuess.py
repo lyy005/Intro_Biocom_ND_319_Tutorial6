60db90738854b328be0b5ca3b95cdb7b565f7852
ayamasaki2011 <ayamasak@nd.edu>
2017-09-29 11:22:56 -0400

    AEY: Add while loop to test user input against answer - doesn't work yet, though

--- NumberGuess.py
+++ NumberGuess.py
@@ -7,3 +7,16 @@ import numpy
 range=numpy.arange(1,101) #define array of possible number choices to exclude 0 and include 100
 answer=numpy.random.choice(range) #generate a random number guess
 
+print("I'm thinking of a number from 1-100. Take a guess!") #Initiate game
+guess=input(prompt="Your guess:") #Store a guess from the user
+
+while guess != answer: #Test if the user input is right
+    if guess < answer: #If not, test whether the guess is lower or higher than the answer
+        print(guess," is lower than my number.")
+        guess=input(prompt="Guess again:")
+    elif guess > answer:
+        print(guess," is higher than my number.")
+        guess=input(prompt="Guess again:")
+
+print(guess," is the right answer!") #If the answer is correct, let the user know
+
