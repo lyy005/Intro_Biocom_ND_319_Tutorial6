f59aece6223a583000786b3dddf71aad9aab9ed5
kkilgoreND <31992787+kkilgoreND@users.noreply.github.com>
2017-09-29 11:48:18 -0400

    Update Exercis06_Part2.py

--- Exercis06_Part2.py
+++ Exercis06_Part2.py
@@ -2,6 +2,7 @@ import random
 guessesTaken=0
 number= random.randint(1,100)
 print('I am thinking of a number between 1 and 100')
+
 while guessesTaken <6:
     print('Make a guess')
     guess= input()
@@ -10,8 +11,8 @@ while guessesTaken <6:
     guessesTaken= guessesTaken + 1
     
     if guess < number:
-        print ('Too low')
+        print ('Higher')
     if guess > number:
-        print ('Too high')
+        print ('Lower')
     if guess == number:
-        print('Good job!')
+        print('Correct!')
