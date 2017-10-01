534b8a90cec61bc39066cea893dab380c68a50f1
omegadan01 <dbruzzes@nd.edi>
2017-10-01 15:44:59 -0400

    q1: cleaned up code

--- exercise6.py
+++ exercise6.py
@@ -19,12 +19,9 @@ for i in range(0,len(bball2),1):
         uw_list.append(i)
     else:
        msu_list.append(i)
-print uw_list
-print msu_list
 
 #add total score columns UW
 for i in range(len(uw_list)):
-    print(uw_list[i])
     if i == 0:
         bball2.loc[uw_list[i], "totalscore"] = bball2.score.loc[uw_list[i]]
     else:
@@ -33,7 +30,6 @@ for i in range(len(uw_list)):
 
 #add total score msu
 for i in range(len(msu_list)):
-    print(msu_list[i])
     if i == 0:
         bball2.loc[msu_list[i], "totalscore"] = bball2.score.loc[msu_list[i]]
     else:
@@ -41,23 +37,6 @@ for i in range(len(msu_list)):
 
 print bball2
 
-
-
-
-#for x in uw list:
-# forthe second value bball2.totalscore[i]= bball2.totalscore[i-1] + bball2.score[i]
-
-
-#Steps remaining:
-#print times in the Time column of B
-#create a running cumulative score for each team in B
-#pseudocode (doesn't work)
-
-#Create blank dataframe
-#import numpy
-#A=numpy.zeros((50,3))
-#B=pandas.DataFrame(A,columns=['Time','ScoreMSU','ScoreUW'])
-#Time = bball.time
 
 #actual plot
 #import matplotlib.pyplot as plt
