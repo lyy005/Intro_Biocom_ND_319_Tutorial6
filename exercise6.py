3b7852a09f6013119c3868de14d2df60c01618d3
omegadan01 <dbruzzes@nd.edi>
2017-10-01 11:57:42 -0400

    q1: got a loop running through the dataframe

--- exercise6.py
+++ exercise6.py
@@ -7,13 +7,8 @@ bball = pandas.read_csv("UWvMSU_1-22-13.txt", sep='\t', lineterminator='\r')
 
 #create blank column totalscore
 bball2=bball.assign(totalscore=0)
-
-#Create blank dataframe
-import numpy
-A=numpy.zeros((50,3))
-B=pandas.DataFrame(A,columns=['Time','ScoreMSU','ScoreUW'])
-Time = bball.time
 print bball2.head(n=5)
+
 
 for i in range(0,len(bball2),1):
     if bball2.team[i]=="UW":
@@ -25,17 +20,16 @@ for i in range(0,len(bball2),1):
 #print times in the Time column of B
 #create a running cumulative score for each team in B
 #pseudocode (doesn't work)
-for team in bball:
-    if team == 'MSU':
-        ScoreMSU + bball.score
-    elif team == 'UW':
-        ScoreUW + bball.score
-    else:
-        print('Something is wrong')
+
+#Create blank dataframe
+#import numpy
+#A=numpy.zeros((50,3))
+#B=pandas.DataFrame(A,columns=['Time','ScoreMSU','ScoreUW'])
+#Time = bball.time
 
 #actual plot
-import matplotlib.pyplot as plt
-plt.plot(Time,ScoreMSU,'g-',Time,ScoreUW,'gray-')
+#import matplotlib.pyplot as plt
+#plt.plot(Time,ScoreMSU,'g-',Time,ScoreUW,'gray-')
 
 #question 2
 
