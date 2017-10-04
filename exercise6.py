e69df5e177dc0cc56d8857ca7934caebc695af27
Mati Nemera <mnemera@nd.edu>
2017-10-03 22:05:19 -0400

    question 2

--- exercise6.py
+++ exercise6.py
@@ -11,3 +11,18 @@
 
 
 #question 2
+import numpy
+rand = numpy.random.randint(1, 101)
+while True:
+    print("I'm thinking of a number 1-100...")
+    guess = input("Guess")
+    if guess < rand:
+        print("Higher")
+        continue
+    elif guess > rand:
+        print("Lower")
+        continue
+    elif guess == rand:
+        print("Correct!")
+        break
+
